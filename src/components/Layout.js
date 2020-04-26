c602647db65da86760634a893b3de59dd9d4a51c
Kate Mills <kate.mills1124@gmail.com>
2020-04-26 15:57:13 -0700

    Added Simple Hero and Banner

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from './Navbar'
 import Footer from './Footer'
+import SimpleHero from './SimpleHero'
 
 import './layout.css'
 
@@ -8,6 +9,7 @@ const Layout = ({ children }) => {
   return (
     <>
       <Navbar />
+      <SimpleHero />
       {children}
       <Footer />
     </>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,16 @@
 import React from 'react'
+import {Link} from 'gatsby'
 import Layout from '../components/Layout'
+import SimpleHero from '../components/SimpleHero'
+import Banner from '../components/Banner'
 
-export default () => <Layout>Hello world</Layout>
+export default () => ( 
+  <Layout>
+    <SimpleHero>
+      <Banner title="continue expoloring"
+              info=" I'm baby tumblr 8-bit hella kickstarter, Single-origin coffee raclette tacos yr asymmetrical narwhal.">
+        <Link className="btn-white" to="/tours">explore tours</Link>
+      </Banner>
+      </SimpleHero>
+  </Layout>
+)
