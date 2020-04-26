e0d235445f0ece758856c056c366928137ae2530
Kate Mills <kate.mills1124@gmail.com>
2020-04-26 12:03:21 -0700

    removed sass - added default bcg

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Navbar from './Navbar'
 import Footer from './Footer'
 
-import './Layout.module.scss'
+import './layout.css'
 
 const Layout = ({ children }) => {
   return (
