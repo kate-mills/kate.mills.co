1d5ec25d65fc4c9e8ebbf02cf567b9cdddbfea7e
Kate Mills <kate.mills1124@gmail.com>
2020-04-26 10:42:00 -0700

    Trouble with capital letter in footer

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import Navbar from './Navbar'
-import Footer from './Footer'
+import Footer from './Myfooter'
 
 const Layout = ({ children }) => {
   return (
