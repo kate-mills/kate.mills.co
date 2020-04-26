ea9d239efb2db22537cdab0934a554e1ce47d44a
Kate Mills <kate.mills1124@gmail.com>
2020-04-26 16:11:33 -0700

    Update Error Page css

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import Layout from '../components/Layout'
+import styles from '../css/error.module.css'
+import Banner from '../components/Banner'
 
 const error = () => {
   return (
     <Layout>
-      <h3>
-        Sorry, we couldn't find that page. <Link to="/">back</Link>
-      </h3>
+      <header className={styles.error}>
+      <Banner title="oops it's a dead end">
+        <Link to ="/" className="btn-white">back to home page</Link>
+      </Banner>
+      </header>
+
     </Layout>
   )
 }
