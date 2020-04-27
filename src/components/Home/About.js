31960f588191f71e3488d360d3a7fc6fbd24f9cc
Kate Mills <kate.mills1124@gmail.com>
2020-04-26 18:28:46 -0700

    New components - Title, About

---
+++ src/components/Home/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Title from '../Title'
+
+const About = () => {
+  return (
+    <div>
+      <Title title="about" subtitle="us" />
+    </div>
+  )
+}
+
+export default About
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,22 @@
 import React from 'react'
-import {Link} from 'gatsby'
+import { Link } from 'gatsby'
 import Layout from '../components/Layout'
 import SimpleHero from '../components/SimpleHero'
 import Banner from '../components/Banner'
+import About from '../components/Home/About'
 
-export default () => ( 
+export default () => (
   <Layout>
     <SimpleHero>
-      <Banner title="continue expoloring"
-              info=" I'm baby tumblr 8-bit hella kickstarter, Single-origin coffee raclette tacos yr asymmetrical narwhal.">
-        <Link className="btn-white" to="/tours">explore tours</Link>
+      <Banner
+        title="continue expoloring"
+        info=" I'm baby tumblr 8-bit hella kickstarter, Single-origin coffee raclette tacos yr asymmetrical narwhal."
+      >
+        <Link className="btn-white" to="/tours">
+          explore tours
+        </Link>
       </Banner>
-      </SimpleHero>
+    </SimpleHero>
+    <About />
   </Layout>
 )
