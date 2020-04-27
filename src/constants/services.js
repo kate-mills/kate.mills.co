b23e16622b68b7c5a4cb82f898f4a99c9e268589
Kate Mills <kate.mills1124@gmail.com>
2020-04-27 14:37:27 -0700

    New component - Services

---
+++ src/constants/services.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { FaWallet, FaTree, FaSocks } from 'react-icons/fa'
+
+export default [
+  {
+    icon: <FaWallet />,
+    title: 'saving money',
+    text:
+      "I'm baby banh mi locavore succulents echo park, thundercats helvetica crucifix.",
+  },
+  {
+    icon: <FaTree />,
+    title: 'endless hiking',
+    text:
+      "I'm baby banh mi locavore succulents echo park, thundercats helvetica crucifix.",
+  },
+  {
+    icon: <FaSocks />,
+    title: 'amazing comfort',
+    text:
+      "I'm baby banh mi locavore succulents echo park, thundercats helvetica crucifix.",
+  },
+]
