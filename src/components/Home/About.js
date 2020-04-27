6ec0f37472685b59b92015824be42dff8a98f6af
Kate Mills <kate.mills1124@gmail.com>
2020-04-27 14:35:41 -0700

    Prettifier Formatter

--- src/components/Home/About.js
+++ src/components/Home/About.js
@@ -10,14 +10,19 @@ const About = () => {
       <div className={styles.aboutCenter}>
         <article className={styles.aboutImg}>
           <div className={styles.imgContainer}>
-            <img src={img} alt="about company"/>
+            <img src={img} alt="about company" />
           </div>
         </article>
         <article className={styles.aboutInfo}>
           <h4>explore the difference</h4>
-          <p>I'm baby fam yuccie wayfarers photo booth, iceland glossier single-origin coffee chia cray synth roof party.</p>
+          <p>
+            I'm baby fam yuccie wayfarers photo booth, iceland glossier
+            single-origin coffee chia cray synth roof party.
+          </p>
           <p>Austin jean shorts cliche food truck bespoke green juice.</p>
-          <button type="button" className="btn-primary">read more</button>
+          <button type="button" className="btn-primary">
+            read more
+          </button>
         </article>
       </div>
     </section>
