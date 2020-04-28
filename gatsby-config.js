ac5bc3134d1d41f8f17a1485c2079ae9bc165764
Kate Mills <kate.mills1124@gmail.com>
2020-04-28 16:42:52 -0700

    About image is optimized with gatsby-image

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,17 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [`gatsby-plugin-sass`, `gatsby-plugin-styled-components`],
+  plugins: [
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-styled-components`,
+  ],
 }
--- src/components/Home/About.js
+++ src/components/Home/About.js
@@ -1,16 +1,34 @@
 import React from 'react'
 import Title from '../Title'
 import styles from '../../css/about.module.css'
-import img from '../../images/defaultBcg.jpeg'
+import { graphql, useStaticQuery } from 'gatsby'
+import Img from 'gatsby-image'
 
+const getAbout = graphql`
+  query AboutImg {
+    aboutImg: file(relativePath: { eq: "defaultBcg.jpeg" }) {
+      childImageSharp {
+        fluid(maxWidth: 600) {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+  }
+`
 const About = () => {
+  const {
+    aboutImg: {
+      childImageSharp: { fluid },
+    },
+  } = useStaticQuery(getAbout)
+
   return (
     <section className={styles.about}>
       <Title title="about" subtitle="us" />
       <div className={styles.aboutCenter}>
         <article className={styles.aboutImg}>
           <div className={styles.imgContainer}>
-            <img src={img} alt="about company" />
+            <Img fluid={fluid} alt="awesome landscape" />
           </div>
         </article>
         <article className={styles.aboutInfo}>
