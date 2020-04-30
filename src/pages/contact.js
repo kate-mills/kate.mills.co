aeb5a526321e547d3ae1a0048125c01483849c3c
Kate Mills <kate.mills1124@gmail.com>
2020-04-30 14:54:30 -0700

    Added contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,11 +2,13 @@ import React from 'react'
 import Layout from '../components/Layout'
 import { graphql } from 'gatsby'
 import StyledHero from '../components/StyledHero'
+import Contact from '../components/Contact/Contact'
 
 const contact = props => {
   return (
     <Layout>
       <StyledHero img={props.data.contactBcg.childImageSharp.fluid} />
+      <Contact />
     </Layout>
   )
 }
