7017dff984d10b8a30f7a33932c57665545c6d7a
Kate Mills <kate.mills1124@gmail.com>
2020-05-01 07:52:49 -0700

    FeaturedTours linking to error page

---
+++ src/components/Home/FeaturedTours.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import Tour from '../Tours/Tour'
+import {graphql, useStaticQuery} from 'gatsby'
+import Title from '../Title'
+import styles from '../../css/items.module.css'
+import AniLink from 'gatsby-plugin-transition-link/AniLink'
+
+
+const getTours = graphql`
+{
+  featuredTours: allContentfulTour(filter: {featured: {eq: true}}) {
+    tours:nodes {
+      name
+      price
+      slug
+      country
+      contentful_id
+      days
+      images {
+        fluid {
+          ...GatsbyContentfulFluid
+        }
+      }
+    }
+  }
+}
+`
+
+const FeaturedTours = () => {
+  const {featuredTours:{tours}} = useStaticQuery(getTours)
+  console.log(tours)
+  return (
+    <section className={styles.tours}>
+      <Title title="featured" subtitle="tours" />
+      <div className={styles.center}>
+        {tours.map((tour) => {
+          return <Tour key={tour.contentful_id} tour={tour} />
+        })}
+      </div>
+      <AniLink fade to="/tours" className="btn-primary">All Tours</AniLink>
+    </section>
+  )
+}
+
+export default FeaturedTours
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import StyledHero from '../components/StyledHero'
 import Banner from '../components/Banner'
 import About from '../components/Home/About'
 import Services from '../components/Home/Services'
+import FeaturedTours from '../components/Home/FeaturedTours'
 
 export default ({ data }) => (
   <Layout>
@@ -21,6 +22,7 @@ export default ({ data }) => (
     </StyledHero>
     <About />
     <Services />
+    <FeaturedTours />
   </Layout>
 )
 export const query = graphql`
