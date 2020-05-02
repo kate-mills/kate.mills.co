ac1300c0badee0330edc401c23ca7ed53f240500
Kate Mills <kate.mills1124@gmail.com>
2020-05-02 15:55:42 -0700

    Facebook card added

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -31,6 +31,17 @@ const SEO = ({ title, description }) => {
     <Helmet title={`${title} | ${siteTitle}`} htmlAttributes={{ lang: 'en' }}>
       <meta name="description" content={description || siteDesc} />
       <meta name="image" content={image} />
+
+      {/* facebook card */}
+      <meta property="og:url" content={siteUrl} />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content={siteTitle} />
+      <meta property="og:description" content={siteDesc} />
+      <meta property="og:image" content={`${siteUrl}${image}`} />
+      <meta property="og:image:width" content="400" />
+      <meta property="og:image:height" content="300" />
+
+      {/* twitter card */}
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:creator" content={twitterUsername} />
       <meta name="twitter:title" content={siteTitle} />
