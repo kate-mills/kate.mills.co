ec1be0610c4ee3f1bb93d75b56f3298ef7003a59
Kate Mills <kate.mills1124@gmail.com>
2020-05-02 13:44:44 -0700

    Blog posts in desc order

--- gatsby-node.js
+++ gatsby-node.js
@@ -12,7 +12,7 @@ exports.createPages = async ({ graphql, actions }) => {
           }
         }
       }
-      posts: allContentfulPost {
+      posts: allContentfulPost(sort: {fields: published, order: DESC}) {
         edges {
           node {
             slug
@@ -38,6 +38,22 @@ exports.createPages = async ({ graphql, actions }) => {
       component: path.resolve('./src/templates/blog-template.js'),
       context: {
         slug: node.slug,
+      },
+    })
+  })
+  // paginate posts
+  const posts = data.posts.edges
+  const postsPerPage = 5
+  const numPages = Math.ceil(posts.length / postsPerPage)
+  const pages = Array.from({ length: numPages }).forEach((_, i) => {
+    createPage({
+      path: i === 0 ? `/blogs` : `/blogs/${i + 1}`,
+      component: path.resolve('./src/templates/blog-list-template.js'),
+      context: {
+        limit: postsPerPage,
+        skip: i * postsPerPage,
+        numPages,
+        currentPage: i + 1,
       },
     })
   })
