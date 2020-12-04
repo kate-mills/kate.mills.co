ab7894e2f4417bea0162dd19130389b224ab2273
Kate Mills <kate.mills1124@gmail.com>
2020-12-03 18:48:25 -0800

    Added font

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,12 +4,12 @@ require('dotenv').config({
 
 module.exports = {
   siteMetadata: {
-    title: `BackRoads`,
-    author: `Kate Mills`,
-    description: `Explore awesome worldwide tours & discover what makes each of them unique. Forget your daily routine & say yes to adventure.`,
+    title: `Ally Digital Solutions`,
+    author: `Ally Digital Solutions`,
+    description: `Beautiful and innovative web solutions for spas & salons.`,
     twitterUsername: `@katie_napa`,
-    image: `/defaultBcg.jpeg`,
-    siteUrl: `https://backroads-2020.netlify.app`,
+    image: `/defaultBg.jpg`,
+    siteUrl: `http://localhost:8000`,
   },
   plugins: [
     {
@@ -26,14 +26,14 @@ module.exports = {
         path: `${__dirname}/src/images/`,
       },
     },
-    {
-      resolve: 'gatsby-plugin-robots-txt',
-      options: {
-        host: 'https://backroads-2020.netlify.app',
-        sitemap: 'https://backroads-2020.netlify.app/sitemap.xml',
-        policy: [{ userAgent: '*', allow: '/' }]
-      }
-    },
+    //{
+      //resolve: 'gatsby-plugin-robots-txt',
+      //options: {
+        //host: 'https://backroads-2020.netlify.app',
+        //sitemap: 'https://backroads-2020.netlify.app/sitemap.xml',
+        //policy: [{ userAgent: '*', allow: '/' }]
+      //}
+    //},
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sass`,
     `gatsby-plugin-sharp`,
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,9 +2,12 @@ import React from 'react'
 import Navbar from './Navbar'
 import Footer from './Footer'
 
+import "fontsource-montserrat";
+import "fontsource-playfair-display-sc";
+
 import './layout.css'
 
-const Layout = ({ children }) => {
+const Layout = ({children }) => {
   return (
     <main>
       <Navbar />
