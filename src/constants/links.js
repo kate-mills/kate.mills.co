b9eac8918ef9895c077c4ac6054e251e25b1c5ce
Kate Mills <kate.mills1124@gmail.com>
2020-12-10 12:33:01 -0800

    Removed unused imports/props

--- src/constants/links.js
+++ src/constants/links.js
@@ -1,38 +1,17 @@
-import React from 'react'
-
-import {AiOutlineHome} from "react-icons/ai"
-
 export default [
   {
-    page: "home",
-    label: "home",
-    url: '/',
-    icon: <AiOutlineHome className="icon"/>,
     path: '/',
     text: 'home',
   },
   {
-    page: "company",
-    label: "about us",
-    url: '/about',
-    icon: <AiOutlineHome className="icon"/>,
     path: '/about',
     text: 'about us',
   },
   {
-
-    page: "company",
-    label: "company portfolio",
-    url: '/portfolio',
-    icon: <AiOutlineHome className="icon"/>,
     path: '/portfolio',
     text: 'portfolio',
   },
   {
-    page: "company",
-    label: "contact us",
-    url: '/contact-us',
-    icon: <AiOutlineHome className="icon"/>,
     path: '/contact-us',
     text: 'contact us',
   },
