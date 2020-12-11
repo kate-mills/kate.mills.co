07f3c1d1f8dbdde7281b0bd8ab18ccd55944e74a
Kate Mills <kate.mills1124@gmail.com>
2020-12-11 08:53:32 -0800

    Raleway font removed

--- root-wrapper.js
+++ root-wrapper.js
@@ -1,7 +1,6 @@
 import React from "react"
 
 import "fontsource-montserrat/400-normal.css"
-import "fontsource-raleway/300-normal.css"
 import './src/components/layout.css'
 
 export const wrapRootElement = ({element}) => {
