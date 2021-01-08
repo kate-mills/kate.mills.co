ac0529f25fa79dbf4f5eb26d93f6b584c7af1c2c
Kate Mills <kate.mills1124@gmail.com>
2021-01-08 09:32:05 -0800

    Consistent page naming (contact-us, about-us)

--- src/constants/links.js
+++ src/constants/links.js
@@ -1,18 +1,32 @@
-export default [
+import { BiCoffee, BiCartAlt, BiChart, BiChat  } from "react-icons/bi";
+import React from 'react';
+
+const sublinks = [
   {
+    page: 'home',
     path: '/',
-    text: 'home',
+    links: [],
   },
   {
-    path: '/about/',
-    text: 'about us',
+    page: 'about us',
+    path: '/about-us',
+    links: [],
   },
   {
+    page: 'portfolio',
     path: '/portfolio/',
-    text: 'portfolio',
+    links: [
+      { label: 'e-commerce', icon: <BiCartAlt />, url: '/e-commerce/' },
+      { label: 'blog', icon: <BiChat />, url: '/blog/' },
+      { label: 'website', icon: <BiChart />, url: '/website/' },
+      { label: 'all projects', icon: <BiCoffee />, url: '/portfolio/' },
+    ],
   },
   {
+    page: 'contact us',
     path: '/contact-us/',
-    text: 'contact us',
-  },
-]
+    links: [],
+  }
+];
+
+export default sublinks;
---
+++ src/pages/about-us.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+import Layout from '../components/Layout'
+import CustomHero from '../components/CustomHero'
+import Banner from '../components/Banner'
+import AboutUs from '../components/Home/AboutUs'
+import Btn from '../components/Btn'
+import SEO from '../components/SEO'
+
+export default ({ data }) => (
+  <Layout>
+    <SEO title="About Us" />
+    <CustomHero img={data.defaultBg.childImageSharp.fluid}>
+      <Banner
+        title="Digital by industry experts"
+        info="located in Napa Valley" >
+        <Btn to="/portfolio/" text="View our portfolio"/>
+      </Banner>
+    </CustomHero>
+    <AboutUs />
+  </Layout>
+)
+export const query = graphql`
+  query {
+    defaultBg: file(relativePath: { eq: "background/Napa.jpeg" }) {
+      childImageSharp {
+        fluid(quality: 100, maxWidth: 4160) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+  }
+`
