3803311668f5bfb2720d53b799f8ee4a6558283e
Kate Mills <helloworldloveksm@icloud.com>
2021-04-29 08:16:46 -0700

    Revert Banner css

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -13,32 +13,33 @@ const Banner = ({ title, info, children, className}) => {
         <span className="title">{title}</span>
         <span className="paragraph">{info}</span>
       </h1>
-      <div>
-        {children}
-      </div>
+      {children}
     </div>
   )
 }
 
 export default styled(Banner)`
   &{
-    color: var(--primaryBlack);
     border: 0;
     color: var(--primaryBlack);
-    width: 80%;
+    margin: 1rem auto 2.5rem;
+    padding: 0 1rem;
+    text-align: center;
+    width 94vw;
   }
   & * {
-    font-family: var(--pFF);
+    color: var(--primaryBlack);
+    font-family: var(--pFF) !important;
     font-weight: 400;
-  }
-  &  .title{
+    white-space: pre-wrap !important;
     margin: 0 auto;
-    margin-bottom: 2rem;
-    display: block;
+    max-width: 90%;
     text-align: center;
-    white-space: pre-wrap;
   }
-  &  .title{
+  & h1{
+    margin-bottom: 1.5rem;
+  }
+  & .title{
     animation-fill-mode: backwards;
     backface-visibility: hidden; /* shaky animation hack */
     display: block;
@@ -47,42 +48,36 @@ export default styled(Banner)`
     padding: 0 1rem;
     margin: 1rem auto;
     text-transform: uppercase;
-    text-align: center;
   }
   & .paragraph{
     animation-fill-mode: backwards;
     backface-visibility: hidden; /* shaky animation hack */
     display: block;
     font-size: 1.5rem;
-    text-align: center;
-    margin-top: 2rem;
   }
   & a{
-    align-items: center;
     animation-fill-mode: backwards;
-    backface-visibility: hidden; /* shaky animation hack */
     background-color: var(--primaryBlack);
     border: 3px solid var(--primaryBlack);
     color: var(--primaryWhite);
-    display: flex;
-    font-size: 1rem;
-    justify-content: center;
-    margin: 5rem auto;
+    display: inline-block;
+    font-size: 1.3rem;
+    max-width: 75vw;
+    padding: .4rem 3rem;
+    text-align: center;
     text-decoration: none;
+    text-transform: capitalize;
     transition: all var(--mainTransition);
-    width: fit-content;
-
     &:hover{
       background-color: transparent;
       color: var(--primaryBlack) !important;
     }
   }
+
   @media (min-width: 768px){
     &{
       padding: 2rem 0;
-    }
-    & a{
-      font-size: 1rem;
+      width: 60vw;
     }
     & .title{
       font-size: 3rem;
@@ -90,6 +85,7 @@ export default styled(Banner)`
     }
     & .paragraph{
       font-size: 2rem;
+      max-width: 75%;
     }
   }
 `
