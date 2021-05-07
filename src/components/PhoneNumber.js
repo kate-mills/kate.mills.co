9ab9d508f424634046fbf58a2e5ddf07fbc06f2f
Kate Mills <helloworldloveksm@icloud.com>
2021-05-07 14:23:24 -0700

    Applying prop.className for transparent bg onHover

--- src/components/PhoneNumber.js
+++ src/components/PhoneNumber.js
@@ -4,7 +4,7 @@ import {Telephone} from '../constants/contact-info'
 
 export default (props)=>{
   return(
-    <a href="tel:707-226-8106" title="Call or Text" className="phone">{Telephone}</a>
+    <a href="tel:707-226-8106" title="Call or Text" className={`${props.className} phone`}>{Telephone}</a>
   )
 }
 
