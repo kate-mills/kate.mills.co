989342b26834e742eeb60a5ca90be94213cd067f
Kate Mills <helloworldloveksm@icloud.com>
2021-05-12 17:33:04 -0700

    seo title is less than 70 char and description added

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,29 +7,35 @@ import AboutUs from '../components/Home/AboutUs'
 import Btn from '../components/Btn'
 import FeaturedWebServices from '../components/Home/FeaturedWebServices'
 import SEO from '../components/SEO'
-import Survey from '../components/Survey'
+import Contact from '../components/Contact/Contact'
 
-export default ({ data }) => (
-  <Layout>
-    <SEO/>
-    <PinkHero img={data.defaultBg.childImageSharp.fluid}>
-      <Banner
-        title="Websites for the beauty industry"
-        info="Elevate your online presence with specialized digital solutions." >
-        <Btn to="/contact-us/" text="Contact us" borderColor="var(--primaryBlack)"/>
-      </Banner>
-    </PinkHero>
-    <AboutUs/> 
-    <FeaturedWebServices 
-      data-sal="zoom-out"
-      data-sal-easing="ease-out"
-      data-sal-duration="500"/>
-    <Survey />
-    <section className="footer-btn">
-      <Btn to="/portfolio/" text="View Our Portfolio" color="var(--primaryColor)" colorful/>
-    </section>
-  </Layout>
-)
+
+export default ({ data }) => {
+  const year = new Date().getFullYear()
+  const t1=`Boost your beauty business`
+  const t2=`in ${year} with a pro web designer`
+  return (
+    <Layout>
+      <SEO title={`${t1} ${t2}`}/>
+      <PinkHero img={data.defaultBg.childImageSharp.fluid}>
+        <Banner
+          title={t1}
+          info={t2}>
+          <Btn to="/contact-us/" text="Let's Chat"/>
+        </Banner>
+      </PinkHero>
+      <AboutUs/>
+      <FeaturedWebServices
+        data-sal="zoom-out"
+        data-sal-easing="ease-out"
+        data-sal-duration="500"/>
+      <Contact/>
+      <section className="footer-btn">
+        <Btn to="/portfolio/" text="View Our Portfolio" color="var(--primaryColor)" colorful/>
+      </section>
+    </Layout>
+  )
+}
 export const query = graphql`
   query {
     defaultBg: file(relativePath: { eq: "Hero/index-hero.jpg" }) {
