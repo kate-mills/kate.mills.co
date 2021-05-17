4263f871b305f6dba9fd3dce6f82fd9462f064f9
Kate Mills <helloworldloveksm@icloud.com>
2021-05-17 15:56:03 -0700

    css module to styled-component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,45 +1,96 @@
 import React from 'react'
-import styles from '../css/footer.module.css'
 import links from '../constants/links'
 import socialIcons from '../constants/social-icons'
 import {Link} from 'gatsby'
 
+import styled from 'styled-components'
+
 const Footer = () => {
   return (
     <>
-    <footer className={styles.footer}>
-      <div className={styles.links}>
+    <FooterWrapper>
+      <div className="links">
         {links.map((item, index) => {
           return (
-            <Link className={styles.textLink} key={index} to={item.path} >
+            <Link key={index} to={item.path} >
               {item.page}
             </Link>
           )
         })}
       </div>
-      <div className={styles.icons}>
-        {socialIcons.map((item, index) => {
-          return (
-            <a
-              key={index}
-              aria-label={item.label}
-              href={item.url}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              {item.icon}
-            </a>
-          )
+      <div className="icons">
+        { socialIcons.map((item, index) => {
+          return <a
+            key={index}
+            aria-label={item.label}
+            href={item.url}
+            target="_blank"
+            rel="noopener noreferrer">
+            {item.icon}</a>
         })}
       </div>
-        <p className={`phone ${styles.phone}`}>(707) 266- 8106</p>
-      <div className={styles.smallFont}>
-        Copyright &copy; {new Date().getFullYear()} All
-        Rights Reserved
+        <p className="phone">(707) 266- 8106</p>
+      <div className="small-font">
+        Copyright &copy; {new Date().getFullYear()} All Rights Reserved
       </div>
-    </footer>
+    </FooterWrapper>
     </>
   )
 }
 
+const FooterWrapper =styled.footer`
+  &{
+    font-family: var(--pFF);
+    font-weight: 300;
+    background-color: var(--primaryBlack);
+    padding: 4rem;
+    margin-top: auto;
+    text-align: center;
+    width: 100%;
+  }
+  & > div{
+    color: var(--primaryWhite);
+  }
+  .links a {
+    color: var(--primaryWhite) !important;
+    display: inline-block;
+    font-size: .8rem;
+    letter-spacing: var(--altSpacing);
+    margin: 1rem 2.5rem;
+    text-decoration: none;
+    transition: var(--mainTransition);
+  }
+  .links a:hover{
+    color: var(--favoriteColor) !important;
+  }
+  .icons{
+    padding: 1.5rem;
+  }
+  .icons a {
+    color: var(--primaryWhite);
+    display: inline-block;
+    font-size: 1.3rem;
+    margin: 1rem;
+    transition: var(--mainTransition);
+  }
+  .icons a:hover {
+    transform: translateY(-1px);
+    color: var(--favoriteColor) !important
+  }
+  .phone{
+    color: var(--primaryWhite);
+  }
+  .phone:hover{
+    cursor: pointer;
+  }
+  .small-font{
+    font-family: var(--pFF);
+    font-size: 0.75rem;
+    margin: 0 0;
+    color: var(--primaryWhite);
+    padding: .5rem .5rem 0;
+    text-align: center;
+  }
+
+`
 export default Footer
