e622e90b1779e53ca476fb762556f188af80004d
Kate Mills <helloworldloveksm@icloud.com>
2021-05-19 14:50:18 -0700

    search photos

--- src/pages/image-search.js
+++ src/pages/image-search.js
@@ -8,33 +8,46 @@ import styled from 'styled-components'
 import {FaSearch} from 'react-icons/fa'
 
 export const mainUrl = `https://api.unsplash.com/photos/`
-export const searchUrl = `https://api.unsplash.com/search/photos/`
+export const searchUrl = `https://api.unsplash.com/search/photos`
 export const clientId = `?client_id=${process.env.GATSBY_UNSPLASH_ACCESS_KEY}`
+export const urlPage = `&page=1`
 
 const ImageSearchPage = ()=>{
   const [loading, setLoading] = React.useState(false)
   const [photos, setPhotos] = React.useState([])
+  const [query, setQuery] = React.useState('spa')
 
-  React.useEffect(()=>{
-    const fetchImages = async ()=>{
-      setLoading(true)
-      let url;
+  const fetchImages = async ()=>{
+    setLoading(true)
+    let url;
+    const urlQuery = `&query=${query}`
+    if (query) {
+      url = `${searchUrl}${clientId}${urlQuery}`
+    } else {
       url = `${mainUrl}${clientId}`
-      try {
-        const response = await fetch(url)
-        const data = await response.json()
-        setPhotos(data)
-        setLoading(false)
-      } catch(err){
-        setLoading(false)
-        console.log(err)
-      }
     }
+    try {
+      const response = await fetch(url)
+      const data = await response.json()
+      if(data.results){
+        setPhotos(data.results)
+      } else{
+        setPhotos(data)
+      }
+      setLoading(false)
+    } catch(err){
+      setLoading(false)
+      console.log(err)
+    }
+  }
+  React.useEffect(()=>{
     fetchImages()
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
+
   const handleSubmit = (e)=>{
     e.preventDefault()
-    console.log('submit form')
+    fetchImages()
   }
   return(
     <Layout>
@@ -43,13 +56,18 @@ const ImageSearchPage = ()=>{
       <ImageSearchWrapper>
 
         <form className="search-form">
-          <input type="text" placeholder="search images" className="form-input"/>
+          <input
+            type='text'
+            placeholder='search'
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+            className='form-input'
+          />
           <button className="submit-btn" type="submit" aria-label="search"
             onClick={handleSubmit}
           ><FaSearch/></button>
         </form>
         <DisplayImages loading={loading} photos={photos}/>
-
       </ImageSearchWrapper>
     </Layout>
   )
