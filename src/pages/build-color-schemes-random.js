617363c681ccf3fc1f408603de311a3ac9034ccb
Kate Mills <helloworldloveksm@icloud.com>
2021-06-08 16:10:34 -0700

    major cleanup

--- src/pages/build-color-schemes-random.js
+++ src/pages/build-color-schemes-random.js
@@ -33,9 +33,9 @@ const ColorSchemes = () => {
       <FullSeo title="Color Schemes" noindex />
       <ColorSchemeWrapper>
           <div className="app-nav">
-            <button tabIndex="0" className="btn generate" onClick={()=>updatePendingColors([...all_colors])}></button>
-            {alert ? <p className="alert">COPIED</p>:<p className="alert"></p>}
-            <button tabIndex="0" className="btn copy" onClick={handleClickCopyColors}></button>
+            <button tabIndex="0" className="btn generate" onClick={()=>updatePendingColors([...all_colors])} aria-label="generate color scheme"></button>
+            {alert ? <span className="alert">COPIED</span>:<span className="alert"></span>}
+            <button tabIndex="0" className="btn copy" onClick={handleClickCopyColors} aria-label="copy color scheme"></button>
           </div>
             <ColorList colors={all_colors}/>
         </ColorSchemeWrapper>
@@ -49,20 +49,22 @@ const ColorSchemeWrapper = styled.div`
   & div.app-nav{
     align-content: center;
     align-items: center;
+    justify-content: center;
     display: flex;
     height: 2.2rem;
     margin: 0;
     padding: 0 2rem;
     width: 100%;
-    > p,> button{
+    > span,> button{
       font-family: var(--altFF);
-      font-weight: 700;
       font-size: 1.2rem;
-      transform: translateX(10px) translateY(0);
+      font-weight: 700;
       line-height: normal;
+      margin: 0;
+      min-width:33.3333%; 
       overflow-wrap: break-word;
+      text-align: center;
       white-space: pre-line;
-      word-spacing: 1px;
     }
     button.btn{
       background: transparent;
@@ -70,47 +72,31 @@ const ColorSchemeWrapper = styled.div`
       color: var(--primaryBlack);
       cursor: pointer;
       letter-spacing: var(--altSpacing);
-      margin: 0;
       outline-color: transparent; 
-      text-align: center;
     }
-    button.btn.generate:before{
-      content:'generate color palette!';
-      min-width: 30%;
-    }
-    button.btn.copy:before{
-      content: 'copy color palette!';
-      min-width: 25%;
-    }
-    .alert{
-      height: 0;
-      min-width: 50%;
-      font-weight: 400;
-      text-align: center;
-    }
+    button.btn.generate{ transform: translateX(0);min-width:40%;}
+    button.btn.copy{ transform: translateX(0); min-width:40%;}
+    button.btn.generate:before{content:'generate color palette!';}
+    button.btn.copy:before{ content:'copy color palette!';}
+    span.alert{font-weight:400;min-width:20%;}
   }
   @media screen and (max-width:1200px){
     & div.app-nav{
-      > p,> button{
-        font-size: 1rem;
-      }
+      > span,> button{font-size:1rem;}
+    }
+  }
+
+  @media screen and (max-width: 646px){
+    & div.app-nav{
+      button.btn.generate:before{content:'generate colors!';}
+      button.btn.copy:before{content:'copy colors!';}
     }
   }
   @media screen and (max-width: 500px){
     & div.app-nav{
-      > p,> button{
-        font-size: .75rem;
-      }
-      button.btn.generate:before{
-        content:'generate colors!';
-      }
-      button.btn.copy:before{
-        content: 'copy colors!';
-      }
-      .alert{
-        transform:translateX(19px) translateY(3px)
-        min-width: 30%;
-      }
+      > span,> button{font-size:.8rem;}
+      button.btn.generate{transform:translateX(-10px);}
+      button.btn.copy{transform:translateX(10px);}
     }
   }
 
