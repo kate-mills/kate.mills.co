d9df3ae4a39e30390b679f1e8f54b68fa93f9b59
Kate Mills <helloworldloveksm@icloud.com>
2021-06-10 14:25:07 -0700

    removed tinycolor import from colors_context

--- src/context/state-colorgen/context/colors_context.js
+++ src/context/state-colorgen/context/colors_context.js
@@ -1,7 +1,6 @@
 import React, { useContext, useEffect, useReducer } from 'react'
 import reducer from '../reducers/colors_reducer'
-import {default_colors} from '../helpers'
-import tinycolor from 'tinycolor2'
+import {default_colors, spaColors} from '../helpers'
 
 import {
   INIT_RANDOM_COLORS,
@@ -14,25 +13,7 @@ const initialState = {
   all_colors: default_colors,
   default_colors,
   theme: {
-    spaColors: [
-      {theme: 'spa', tiny: tinycolor('#eeddcc')}, // Pink
-      {theme: 'spa', tiny:tinycolor('#b4b387')},  // Olive
-      {theme: 'spa', tiny: tinycolor('#d3c38b')}, // yellowOlivish 
-      {theme: 'spa', tiny: tinycolor('#778aab')}, // Blue
-      {theme: 'spa', tiny: tinycolor('#677988')}, // dkBlue
-      {theme: 'spa', tiny: tinycolor('#8f9ea2')}, // medBlue
-      {theme: 'spa', tiny: tinycolor('#a8b8c3')}, // litBlue
-      {theme: 'spa', tiny: tinycolor('#bcaaaa')}, // Beige
-      {theme: 'spa', tiny: tinycolor('#ddbb98')}, // Peach
-      {theme: 'spa', tiny: tinycolor('#988693')}, // Purplish
-      {theme: 'spa', tiny: tinycolor('#82967b')}, // darkGreenish
-      {theme: 'spa', tiny: tinycolor('#9fb996')}, // mediGreenish
-      {theme: 'spa', tiny: tinycolor('#c0cfaa')}, // litGreenish
-      {theme: 'spa', tiny: tinycolor('#adb3a9')}, // greyish
-      {theme: 'spa', tiny: tinycolor('#96b2a6')}, // prettyGreen
-      {theme: 'spa', tiny: tinycolor('#edeee8')}, // white
-      {theme: 'spa', tiny: tinycolor('#e0dcd0')}, // taupish
-    ],
+    spaColors,
   }
 }
 
--- src/context/state-colorgen/helpers.js
+++ src/context/state-colorgen/helpers.js
@@ -1,24 +1,5 @@
 import tinycolor from 'tinycolor2'
 
-const default_colors = [
-  { id: 0, hex: "#1ae0d9", onHold: false },
-  { id: 1, hex: "#65f7df", onHold: false },
-  { id: 2, hex: "#c5ffaf", onHold: false },
-  { id: 3, hex: "#8fbf35", onHold: false },
-  { id: 4, hex: "#094d9b", onHold: false },
-  { id: 5, hex: "#017720", onHold: false },
-]
-
-const top_half_of_app_height = 114;
-
-
-const getFmtDate = ()=>{
-  const d = new Date()
-  let mo = d.getMonth()+1
-  let dy = d.getDate() 
-  let yr = String(d.getFullYear()).slice(2)
-  return `${mo}-${dy}-${yr}`
-}
 const copyColorScheme = (lst)=>{
   let dt = getFmtDate()
   let hexes = [`RGB hex codes. Color scheme created ${dt}.\n`]
@@ -36,18 +17,24 @@ const copyColorScheme = (lst)=>{
   return
 }
 
-const getReadableColorFromHex = (hex) => {
-  let tiny = tinycolor(hex)
-  return tiny.isLight()?'black':'white'
+const default_colors = [
+  { id: 0, hex: "#1ae0d9", onHold: false },
+  { id: 1, hex: "#65f7df", onHold: false },
+  { id: 2, hex: "#c5ffaf", onHold: false },
+  { id: 3, hex: "#8fbf35", onHold: false },
+  { id: 4, hex: "#094d9b", onHold: false },
+  { id: 5, hex: "#017720", onHold: false },
+]
+
+const getFmtDate = ()=>{
+  const d = new Date()
+  let mo = d.getMonth()+1
+  let dy = d.getDate() 
+  let yr = String(d.getFullYear()).slice(2)
+  return `${mo}-${dy}-${yr}`
 }
 
-const getRandomInt = (max) => {
-// will never return max
-  return Math.floor(Math.random() * max) 
-}
-
-
-let getRandomHex= () => {
+const getRandomHex=()=> {
   let hexvalues = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"]
   let hex = "#"
   for (let i = 0; i < 6; i++){
@@ -59,12 +46,44 @@ let getRandomHex= () => {
   return hex
 }
 
+const getRandomInt = (max) => {
+// will never return max
+  return Math.floor(Math.random() * max) 
+}
+
+const getReadableColorFromHex = (hex) => {
+  let tiny = tinycolor(hex)
+  return tiny.isLight()?'black':'white'
+}
+
+const spaColors = [
+  {theme: 'spa', tiny: tinycolor('#eeddcc')}, // Pink
+  {theme: 'spa', tiny:tinycolor('#b4b387')},  // Olive
+  {theme: 'spa', tiny: tinycolor('#d3c38b')}, // yellowOlivish
+  {theme: 'spa', tiny: tinycolor('#778aab')}, // Blue
+  {theme: 'spa', tiny: tinycolor('#677988')}, // dkBlue
+  {theme: 'spa', tiny: tinycolor('#8f9ea2')}, // medBlue
+  {theme: 'spa', tiny: tinycolor('#a8b8c3')}, // litBlue
+  {theme: 'spa', tiny: tinycolor('#bcaaaa')}, // Beige
+  {theme: 'spa', tiny: tinycolor('#ddbb98')}, // Peach
+  {theme: 'spa', tiny: tinycolor('#988693')}, // Purplish
+  {theme: 'spa', tiny: tinycolor('#82967b')}, // darkGreenish
+  {theme: 'spa', tiny: tinycolor('#9fb996')}, // mediGreenish
+  {theme: 'spa', tiny: tinycolor('#c0cfaa')}, // litGreenish
+  {theme: 'spa', tiny: tinycolor('#adb3a9')}, // greyish
+  {theme: 'spa', tiny: tinycolor('#96b2a6')}, // prettyGreen
+  {theme: 'spa', tiny: tinycolor('#edeee8')}, // white
+  {theme: 'spa', tiny: tinycolor('#e0dcd0')}, // taupish
+]
+
+const top_half_of_app_height = 114;
 
 export {
-  default_colors,
-  top_half_of_app_height,
   copyColorScheme,
-  getReadableColorFromHex,
-  getRandomInt,
+  default_colors,
   getRandomHex,
+  getRandomInt,
+  getReadableColorFromHex,
+  spaColors,
+  top_half_of_app_height,
 }
