a942e4228c2066466770be42fa2648e2253aa3d0
Kate Mills <helloworldloveksm@icloud.com>
2022-10-31 15:52:16 -0700

    portfolio web config

--- config/website.js
+++ config/website.js
@@ -3,18 +3,18 @@
 
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
-  title: 'Ally Digital Solutions', // Navigation and Site Title
-  titleAlt: 'Ally Digital Solutions', // Title for JSONLD
+  title: 'Kate Mills - Web Developer', // Navigation and Site Title
+  titleAlt: 'Kate Mills - Web Developer', // Title for JSONLD
   description: '',
   headline: '', // Headline for schema.org JSONLD
-  url: 'https://allydigitalsolutions.com', // Domain of your site. No trailing slash!
+  url: 'https://katemillsco.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   logo: '/images/icon.png', // Used for SEO
   ogLanguage: 'en_US', // Facebook Language
 
   // JSONLD / Manifest
   favicon: 'static/favicon.png', // Used for manifest favicon generation
-  shortName: 'allydigital', // shortname for manifest. MUST be shorter than 12 characters
+  shortName: 'kate mills', // shortname for manifest. MUST be shorter than 12 characters
   author: 'kate-mills', // Author for schemaORGJSONLD
   themeColor: '#f5e7e9',
   backgroundColor: '#FFFF',
