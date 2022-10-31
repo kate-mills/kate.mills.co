3a1e12864cc88bbd748e83667ebe7733f4d1c942
Kate Mills <helloworldloveksm@icloud.com>
2022-10-31 13:52:18 -0700

    only type out 1 line of text

--- src/components/Hero/Banner.js
+++ src/components/Hero/Banner.js
@@ -1,35 +1,47 @@
 import React from 'react'
 import styled from 'styled-components'
-import Typed from 'react-typed';
+import Typed from 'react-typed'
 
-const Banner = ({ title, showTyping, overrideTxt, children, className}) => {
-  const typeit = [
-    `Python, Javascript, HTML5, CSS3, Node.js, React, Gatsby, jQuery, Bootstrap, Sass, Linux Essentials Certified, and more`,
-  ]
+const Banner = ({ title, showTyping, overrideTxt, children, className }) => {
+  const typeit = [` Sass, Linux Essentials Certified, and more`]
   return (
     <div className={className}>
       <div className="parent-div">
-      <h1 className="title-heading">{title}</h1>
-        {!!showTyping
-          ?<Typed className="title-typing" strings={typeit} typeSpeed={40} backSpeed={50} />
-          :<div className="katemills">{overrideTxt}</div>
-        }
+        <h1 className="title-heading">{title}</h1>
+
+        {!!showTyping ? (
+          <>
+            {' '}
+            <span className="title-typing">
+              Python, Javascript, HTML5, CSS3, Node.js, React, Gatsby, jQuery,
+              Bootstrap,
+            </span>
+            <Typed
+              className="title-typing"
+              strings={typeit}
+              typeSpeed={40}
+              backSpeed={50}
+            />
+          </>
+        ) : (
+          <div className="katemills">{overrideTxt}</div>
+        )}
       </div>
       <div
-          className="child-div"
-          data-sal="zoom-in"
-          data-sal-easing="ease"
-          data-sal-duration="100">
-      {children}
+        className="child-div"
+        data-sal="zoom-in"
+        data-sal-easing="ease"
+        data-sal-duration="100"
+      >
+        {children}
       </div>
     </div>
   )
 }
-Banner.defaultProps={
-}
+Banner.defaultProps = {}
 
 export default styled(Banner)`
-  &{
+  & {
     display: flex;
     align-items: center;
     flex-direction: column;
@@ -55,18 +67,18 @@ export default styled(Banner)`
       margin: 0 auto;
       position: relative;
     }
-    div.parent-div{
+    div.parent-div {
       padding-top: 2rem;
       width: 100vw;
     }
-    div.child-div{
+    div.child-div {
       width: 100vw;
       padding: 1rem;
       margin-bottom: 1rem;
     }
   }
   @media screen and (min-width: 768px) {
-    &{
+    & {
       h1.title-heading {
         margin-top: 3rem;
         font-size: 3.5rem;
