b0a55512f5909a854e4b051c2f0482a7c830ffc8
Kate Mills <helloworldloveksm@icloud.com>
2023-05-01 18:15:10 -0700

    updated links

--- src/constants/links.js
+++ src/constants/links.js
@@ -13,8 +13,8 @@ const sublinks = [
     cls: 'no-mobile show-dt',
     links: [
       { label: 'Color Scheme Generator', url: '/color-scheme-generator', cls: '' },
-      { label: 'Forty Tints & Shades', url: '/forty-tints-and-shades', cls: '' },
-      { label: 'Search Pic Keep Pic!', url: '/search-pic-keep-pic', cls: '' },
+      { label: 'Tints & Shades', url: '/tints-and-shades', cls: '' },
+      { label: 'Download Images', url: '/download-images', cls: '' },
     ],
   },
 ]
